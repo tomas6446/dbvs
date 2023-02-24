da50cac1aded6777bd6e7f74210f9514fbe02568
tomas6446 <tomas.kozakas@gmail.com>
2023-02-24 18:27:45 +0200

    Add SELECT to find highest count of books taken in average

--- 1/commands.sql
+++ 1/commands.sql
@@ -5,25 +5,26 @@ FROM stud.skaitytojas,
 WHERE stud.skaitytojas.nr = stud.egzempliorius.skaitytojas;
 
 -- 2
-SELECT s.vardas, s.pavarde, a.isbn, s2.vardas, s2.pavarde, b.isbn
+SELECT s1.vardas, s1.pavarde, s2.vardas, s2.pavarde, a.isbn, b.isbn
 FROM stud.egzempliorius a
-         INNER JOIN stud.skaitytojas s
-                    ON a.skaitytojas = s.nr
-         INNER JOIN stud.egzempliorius b
-                    ON a.isbn = b.isbn
-                        AND a.skaitytojas > 0
-                        AND b.skaitytojas > 0
-                        AND a.skaitytojas != b.skaitytojas
-         INNER JOIN stud.skaitytojas s2
+JOIN stud.egzempliorius b ON a.isbn = b.isbn
+JOIN stud.skaitytojas s1 ON s1.nr = a.skaitytojas
+JOIN stud.skaitytojas s2 ON s2.nr = b.skaitytojas
+WHERE s1.vardas != s2.vardas AND s1.pavarde != s2.pavarde;
 
 -- 3
 SELECT DISTINCT a.metai, count(a.metai)
 FROM stud.knyga a
-group by a.metai;
-
+GROUP BY a.metai;
 
 -- 4
-SELECT nr, isbn, skaitytojas, paimta, grazinti
-FROM stud.egzempliorius
-
-
+SELECT a.paimta, count(a.paimta)
+FROM stud.egzempliorius a
+GROUP BY a.paimta
+HAVING count(a.paimta) > (SELECT avg(quantity)
+                    FROM (
+                        SELECT a.paimta, count(a.paimta) as quantity
+                        FROM stud.egzempliorius a
+                        WHERE a.paimta IS NOT NULL
+                        GROUP BY a.paimta
+                    ) as counts);
