1f9959ee493ba854fb2745bf87602bdd79feaa79
tomas <tomas.kozakas@gmail.com>
2023-03-23 13:54:43 +0200

    Add new queries for 4th and 5th

--- 1 lab/task/4.sql
+++ 1 lab/task/4.sql
@@ -10,12 +10,19 @@ HAVING count(a.paimta) > (SELECT avg(quantity)
 WITH counts AS (SELECT a.paimta, count(a.paimta) as quantity
                 FROM stud.egzempliorius a
                 WHERE a.paimta IS NOT NULL
-                GROUP BY a.paimta)
+                GROUP BY a.paimta),
+     min_paimta AS (SELECT min(quantity) as minimum
+                    FROM counts)
 SELECT a.paimta, count(a.paimta)
 FROM stud.egzempliorius a
-         JOIN counts on counts.paimta = a.paimta
+         JOIN counts ON counts.paimta = a.paimta
 GROUP BY a.paimta
-HAVING count(a.paimta) > (SELECT count(quantity) FROM counts)
+HAVING count(a.paimta) > (SELECT avg(quantity) FROM counts)
+    OR count(a.paimta) = (SELECT minimum FROM min_paimta);
+
+
+
+
 
 
 
--- 1 lab/task/5.sql
+++ 1 lab/task/5.sql
@@ -1,10 +1,14 @@
-SELECT table_name
-FROM information_schema.tables
-WHERE table_schema = 'information_schema'
-  AND table_type = 'BASE TABLE'
-  AND NOT EXISTS(
-        SELECT constraint_name
-        FROM information_schema.table_constraints
-        WHERE table_name = tables.table_name
-          AND constraint_type = 'PRIMARY KEY'
-    );
+SELECT DISTINCT a.table_schema, a.table_name, b.constraint_type LIKE '%PRIMARY KEY' as has_key, b.constraint_type, count(a.table_name)
+FROM information_schema.tables a
+         JOIN information_schema.table_constraints b ON a.table_name = b.table_name AND b.constraint_type = 'PRIMARY KEY'
+WHERE a.table_name NOT IN (SELECT table_name
+                           FROM information_schema.table_constraints
+                           WHERE table_schema = 'stud'
+                             AND constraint_type = 'PRIMARY KEY')
+group by a.table_name, a.table_schema, b.constraint_type;
+-- kiekvienai lentelei:
+-- schema,
+-- pavadinima,
+-- kiek is viso stulpelio,
+-- turi rakta ar neturi,
+-- kiek yra stulpeliu kuriu tipas yra skaicius
