903d08f34ed8d6d9a0f8b03db9abc0d8c05750cb
tomas <tomas.kozakas@gmail.com>
2023-03-23 13:59:12 +0200

    5th query

--- 1 lab/task/5.sql
+++ 1 lab/task/5.sql
@@ -1,11 +1,11 @@
-SELECT DISTINCT a.table_schema, a.table_name, b.constraint_type LIKE '%PRIMARY KEY' as has_key, b.constraint_type, count(a.table_name)
+SELECT a.table_schema, a.table_name
 FROM information_schema.tables a
-         JOIN information_schema.table_constraints b ON a.table_name = b.table_name AND b.constraint_type = 'PRIMARY KEY'
 WHERE a.table_name NOT IN (SELECT table_name
                            FROM information_schema.table_constraints
                            WHERE table_schema = 'stud'
                              AND constraint_type = 'PRIMARY KEY')
-group by a.table_name, a.table_schema, b.constraint_type;
+group by a.table_name, a.table_schema;
+
 -- kiekvienai lentelei:
 -- schema,
 -- pavadinima,
